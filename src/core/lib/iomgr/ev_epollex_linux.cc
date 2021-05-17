60c986e15cae70aade721d26badabab1f822fdd6
Nikolai Kochetov <KochetovNicolai@users.noreply.github.com>
2021-05-17 17:18:47 +0300

    Update ev_epollex_linux.cc

--- src/core/lib/iomgr/ev_epollex_linux.cc
+++ src/core/lib/iomgr/ev_epollex_linux.cc
@@ -565,6 +565,8 @@ static grpc_error* pollable_create(pollable_type type, pollable** p) {
   }
   GRPC_FD_TRACE("Pollable_create: created epfd: %d (type: %d)", epfd, type);
   *p = static_cast<pollable*>(gpr_malloc(sizeof(**p)));
+  /// Zero memory to suppress msan.
+  memset((*p)->events, 0, sizeof(epoll_event) * MAX_EPOLL_EVENTS);
   grpc_error* err = grpc_wakeup_fd_init(&(*p)->wakeup);
   if (err != GRPC_ERROR_NONE) {
     GRPC_FD_TRACE(
